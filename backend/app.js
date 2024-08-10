4cf31df0fa4aa46f67d2569e96e1d68e1d68207b
Pranay Kumar Parate <pranayparate53@gmail.com>
2024-08-10 15:10:07 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+import express, { Router } from "express";
+import { ToastContainer } from "react-toastify";
+
+import messageRouter from "./router/messageRouter.js";
+import { config } from "dotenv";
+config({ path: "./config.env"});
+import { dbConnection } from "./database/dbConnection.js";
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
